486c1689c58225394c29686178ab9ef80a350319
Ranjan, Nishit (US - Bengaluru) <nishiranjan@deloitte.com>
2018-09-23 18:44:47 +0530

    initial setup of redux for facebook auth

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {createBottomTabNavigator,createStackNavigator} from 'react-navigation';
+import {Provider} from 'react-redux';
 
+import store from './store';
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import MapScreen from './screens/MapScreen';
@@ -29,8 +31,9 @@ export default class App extends React.Component {
 
    });
     return (
-      
+      <Provider store={store}>
        <MainNavigator/>
+       </Provider>
       
     );
   }
